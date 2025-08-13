4a4888a20f0c6d56d4d12b02fded9d3f044a73a0
MewKay <miorakanto25z@gmail.com>
2025-08-13 17:06:07 +0300

    Create sign up form

---
+++ src/features/signup-form/signup-form.jsx
@@ -0,0 +1,77 @@
+import Input from "@/components/input/input";
+import ranges from "@/lib/validation/ranges";
+import signUpSchema from "@/lib/validation/schema/signup-schema";
+import { useState } from "react";
+import { Form } from "react-router-dom";
+
+const SignUpForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [authorPassword, setAuthorPassword] = useState("");
+
+  const { errors, isFormValid } = signUpSchema.validateInputs({
+    username,
+    password,
+    confirm_password: confirmPassword,
+    author_password: authorPassword,
+  });
+
+  return (
+    <Form method="post">
+      <Input
+        type="text"
+        name="username"
+        value={username}
+        setValue={setUsername}
+        errorMessage={errors.username}
+        minLength={ranges.username.min}
+        maxLength={ranges.username.max}
+        required
+      >
+        Username
+      </Input>
+
+      <Input
+        type="password"
+        name="password"
+        value={password}
+        setValue={setPassword}
+        errorMessage={errors.password}
+        minLength={ranges.password.min}
+        maxLength={ranges.password.max}
+        required
+      >
+        Password
+      </Input>
+
+      <Input
+        type="password"
+        name="confirm_password"
+        value={confirmPassword}
+        setValue={setConfirmPassword}
+        errorMessage={errors.confirm_password}
+        minLength={ranges.password.min}
+        maxLength={ranges.password.max}
+        required
+      >
+        Confirm Password
+      </Input>
+
+      <Input
+        type="password"
+        name="author_password"
+        value={authorPassword}
+        setValue={setAuthorPassword}
+        errorMessage={errors.author_password}
+        required
+      >
+        Authorization Pass
+      </Input>
+
+      <button disabled={!isFormValid}>Sign Up</button>
+    </Form>
+  );
+};
+
+export default SignUpForm;
