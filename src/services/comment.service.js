b170019b1208500dfa3eb2c4034ca54d044db0a4
MewKay <miorakanto25z@gmail.com>
2025-08-23 08:44:42 +0300

    Add deleteOne to comment service

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -2,6 +2,8 @@ import api from "./api-client";
 
 const commentService = {
   getAllByPostId: (postId) => api.get(`/posts/${postId}/comments`),
+  deleteOne: (postId, commentId) =>
+    api.delete(`/posts/${postId}/comments/${commentId}`),
 };
 
 export default commentService;
