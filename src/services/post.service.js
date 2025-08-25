cd47443d8eedb85404a96cf28dfc2ce146ef67aa
MewKay <miorakanto25z@gmail.com>
2025-08-25 15:11:45 +0300

    Add createPost service

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -5,6 +5,7 @@ const postService = {
     api.get(`/authors/${authorId}/posts`, token),
   getAuthorPost: ({ authorId, postId }, token) =>
     api.get(`/authors/${authorId}/posts/${postId}`, token),
+  createPost: (body, token) => api.post(`/posts`, body, token),
 };
 
 export default postService;
