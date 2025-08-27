3c25771af22092e99dae639a95acff760c665658
MewKay <miorakanto25z@gmail.com>
2025-08-27 21:01:22 +0300

    Create update and delete post service

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -6,6 +6,8 @@ const postService = {
   getAuthorPost: ({ authorId, postId }, token) =>
     api.get(`/authors/${authorId}/posts/${postId}`, token),
   createPost: (body, token) => api.post(`/posts`, body, token),
+  updatePost: (postId, body, token) => api.put(`/posts/${postId}`, body, token),
+  deletePost: (postId, token) => api.delete(`/posts/${postId}`, token),
 };
 
 export default postService;
