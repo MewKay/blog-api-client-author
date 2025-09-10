9d1737cf585cae14c68103c53d34c2203ca0b05d
MewKay <miorakanto25z@gmail.com>
2025-09-10 17:02:07 +0300

    Style new post link

--- src/app/pages/home/home.jsx
+++ src/app/pages/home/home.jsx
@@ -1,13 +1,16 @@
 import paths from "@/app/routes/paths";
 import PostList from "@/features/post-list/post-list";
 import { Link, useLoaderData } from "react-router-dom";
+import styles from "./home.module.css";
 
 const Home = () => {
   const { posts } = useLoaderData();
 
   return (
     <main>
-      <Link to={paths.newBlogPost.path}>CREATE NEW POST</Link>
+      <Link to={paths.newBlogPost.path} className={styles.newPostLink}>
+        CREATE NEW POST
+      </Link>
       <PostList posts={posts} />
     </main>
   );
