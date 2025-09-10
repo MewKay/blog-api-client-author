f6f49d3db232ef77ea5ad5feaf3cb81896724c36
MewKay <miorakanto25z@gmail.com>
2025-09-10 18:28:01 +0300

    Style post item

--- src/features/post-list/post-item/post-item.jsx
+++ src/features/post-list/post-item/post-item.jsx
@@ -2,6 +2,8 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { formatPostDate } from "./post-item.util";
 import sqids from "@/lib/sqids";
+import styles from "./post-item.module.css";
+import paths from "@/app/routes/paths";
 
 const PostItem = ({ post }) => {
   const { id, title, preview, created_at, edited_at, is_published, slug } =
@@ -14,15 +16,27 @@ const PostItem = ({ post }) => {
 
   return (
     <li>
-      <Link to={`/posts/${encodedId}/${slug}`}>
-        <h3>{title}</h3>
-        <div>
-          <p>Created : {postDate}</p>
-          {wasPostUpdated && <p>&middot; Last Update : {postUpdateDate}</p>}
-        </div>
-        <p>{preview}</p>
-        {is_published ? <p>✅️ Published</p> : <p>❎️ Unpublished</p>}
-      </Link>
+      <div className={styles.postItem}>
+        <Link
+          to={paths.blogPost.getHref(encodedId, slug)}
+          className={styles.postLink}
+        >
+          <h3>{title}</h3>
+          <div className={styles.dateContainer}>
+            <p>Created : {postDate}</p>
+            {wasPostUpdated && (
+              <>
+                <p>&middot;</p>
+                <p>Last Update : {postUpdateDate}</p>
+              </>
+            )}
+          </div>
+          <p className={styles.preview}>{preview}</p>
+          <div className={styles.publicationContainer}>
+            {is_published ? <p>✅️ Published</p> : <p>❎️ Unpublished</p>}
+          </div>
+        </Link>
+      </div>
     </li>
   );
 };
