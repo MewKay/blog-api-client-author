0bd8b67118e69b48effdfb96a660cd13b4418a65
MewKay <miorakanto25z@gmail.com>
2025-09-12 20:41:18 +0300

    Style input component

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,24 +1,30 @@
 import PropTypes from "prop-types";
 import { forwardRef } from "react";
+import styles from "./input.module.css";
 
 const Input = forwardRef(function Input(
   { children, value, setValue, errorMessage, ...inputProps },
   ref,
 ) {
-  const isValueNotEmpty = value !== "";
+  const isErrorMessageVisible = value !== "" && errorMessage;
 
   return (
-    <div>
-      <label>
+    <div className={styles.inputContainer}>
+      <label className={styles.inputLabelContainer}>
         {children}
         <input
+          className={styles.inputText}
           value={value}
           onChange={(event) => setValue(event.target.value)}
           {...inputProps}
           ref={ref}
         />
       </label>
-      <p>{isValueNotEmpty && errorMessage}</p>
+      <p
+        className={`${styles.errorMessage}${!isErrorMessageVisible ? " " + styles.hidden : ""}`}
+      >
+        {errorMessage}
+      </p>
     </div>
   );
 });
