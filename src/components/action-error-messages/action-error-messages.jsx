8caa29296e8a9c7484bd7c194f9224fcfcfb8279
MewKay <miorakanto25z@gmail.com>
2025-09-22 13:06:42 +0300

    Add styling to action error messages

--- src/components/action-error-messages/action-error-messages.jsx
+++ src/components/action-error-messages/action-error-messages.jsx
@@ -1,18 +1,23 @@
 import PropTypes from "prop-types";
+import styles from "./action-error-message.module.css";
+import { AlertCircle } from "lucide-react";
 
 const ActionErrorMessages = ({ actionData }) => {
   return (
     <>
       {actionData && (
-        <ul>
-          {Array.isArray(actionData.error) ? (
-            actionData.error.map((errorMessage, index) => (
-              <li key={index}>{errorMessage}</li>
-            ))
-          ) : (
-            <li>{actionData.error}</li>
-          )}
-        </ul>
+        <div className={styles.errorContainer} role="alert">
+          <AlertCircle />
+          <ul className={styles.errorList}>
+            {Array.isArray(actionData.error) ? (
+              actionData.error.map((errorMessage, index) => (
+                <li key={index}>{errorMessage}</li>
+              ))
+            ) : (
+              <li>{actionData.error}</li>
+            )}
+          </ul>
+        </div>
       )}
     </>
   );
